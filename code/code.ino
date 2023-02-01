6b730909fc35b1585e89312ca5e436fe860a8a54
rachung2510 <rachung2510@gmail.com>
2023-02-01 00:16:34 +0000

    simple code to store acc data to SD & add acc thresholds

--- code/code.ino
+++ code/code.ino
@@ -1,5 +1,7 @@
 #include "ICM20600.h"
 #include <Wire.h>
+#include <SD.h>
+#include <SPI.h>
 
 ICM20600 icm20600(true);
 
@@ -7,18 +9,29 @@ ICM20600 icm20600(true);
 #define FLEXPIN1 A1
 #define FLEXPIN2 A2
 #define FLEXPIN3 A3
-
 #define SDA A4
 #define SCL A5
+#define SD_ChipSelectPin 10
 
+// flex sensor values
 float indexVal;
 float middleVal;
 float ringVal;
 float pinkyVal;
 
+// IMU values
 int accX;
 int accY;
 int accZ;
+int sound_activated = 0; // boolean
+int ACC_THRESH_LOW = -1500;
+int ACC_THRESH_HIGH = -600;
+int counter = 0;
+
+// writing to SD card
+int STORE_DATA = 0; // boolean
+File dataFile;
+String filename = "acc_data.csv";
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,62 +42,70 @@ void setup() {
   Serial.begin(9600);
   icm20600.initialize();
   // icm20600.reset();
-
   // Serial.print("DeviceID: ");
   // Serial.println(icm20600.getDeviceID(), HEX);
+
+  // Check if the card is present and can be initialized:
+  if (STORE_DATA == 1) {
+    if (!SD.begin(SD_ChipSelectPin)) {  
+      Serial.println("SD fail");  
+      return;
+    }
+    // Serial.println("SD ok");
+    dataFile = SD.open(filename, FILE_WRITE);  
+    dataFile.println("accX,accY,accZ");
+  }
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  indexVal = analogRead(FLEXPIN0);
-  middleVal = analogRead(FLEXPIN1);
-  ringVal = analogRead(FLEXPIN2);
-  pinkyVal = analogRead(FLEXPIN3);
+  // indexVal = analogRead(FLEXPIN0);
+  // middleVal = analogRead(FLEXPIN1);
+  // ringVal = analogRead(FLEXPIN2);
+  // pinkyVal = analogRead(FLEXPIN3);
+
+  // Serial.print("Index:");
+  // Serial.print(indexVal);
+  // Serial.print(",Middle:");
+  // Serial.println(middleVal);
 
   accX = icm20600.getAccelerationX();
   accY = icm20600.getAccelerationY();
   accZ = icm20600.getAccelerationZ();
 
-  // Serial.print("Index:");
-  // Serial.print(indexVal);
-  // printVal("Index", indexVal, "");
-  // printVal("Middle", middleVal, "");
-  // printVal("Ring", ringVal, "");
-  // printVal("Pinky", pinkyVal, "");
-  // Serial.println("");
+  // Serial.print("accX:");
+  // Serial.println(accX);
+  // Serial.print(",accY:");
+  // Serial.print(accY);
+  // Serial.print(",accZ:");
+  // Serial.println(accZ);
   
-  // Serial.println("A (mg): ");
-  // Serial.print("X:");
-  Serial.print(accX);
-  Serial.print(",");
-  Serial.print(accY);
-  Serial.print(",");
-  Serial.println(accZ);
+  if (STORE_DATA == 0) {
+    if ((accX < ACC_THRESH_LOW) && (sound_activated == 0)) {
+      counter++;
+      Serial.print("PLAY SOUND ");
+      Serial.println(counter);
+      sound_activated = 1;
+    }
+    if (accX > ACC_THRESH_HIGH) {
+      sound_activated = 0;
+    }
+  } else {
+    // write to CSV
+    dataFile.print(accX);
+    dataFile.print(",");
+    dataFile.print(accY);
+    dataFile.print(",");
+    dataFile.println(accZ);  
 
-  // Serial.println("-------------------------");
-  // delay(2000);
+    if (Serial.available()) {
+      if (Serial.read() == 'q') {
+        dataFile.close();
+      }
+    }
+  }
 
 }
 
-void printVal(String name, float val, String unit) {
-  Serial.print(name);
-  Serial.print(": ");
-  Serial.print(val);
-  Serial.print(" ");
-  Serial.println(unit);
-}
-
-void printImuVals() {
-  Serial.println("Acceleroscope");
-
-  printVal("X", icm20600.getAccelerationX(), "mg");
-  printVal("Y", icm20600.getAccelerationY(), "mg");
-  printVal("Z", icm20600.getAccelerationZ(), "mg");
-
-  Serial.println("Gyroscope");
-  // printVal("X", icm20600.getGyroscopeX(), "dps");
-  // printVal("Y", icm20600.getGyroscopeY(), "dps");
-  // printVal("Z", icm20600.getGyroscopeZ(), "dps");
-}
