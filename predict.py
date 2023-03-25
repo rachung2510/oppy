438bf1050ec06620425796870fc7570b56485e5c
rachung2510 <rachung2510@gmail.com>
2023-03-25 14:07:08 +0000

    add realtime prediction for left hand

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import os
+# Ignore tensorflow warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import argparse
 import serial
@@ -7,17 +8,18 @@ import threading
 import numpy as np
 from tensorflow import keras
 from scipy.ndimage import uniform_filter1d
-# import pyglet
-import keyboard
+import pyglet
+# import keyboard
 
 def read_serial(model, HAND):
 	global event
 
 	gesture = "4-4" # defaults to crash
-	prev_gesture = "4-4"
+	prev_gesture = gesture
 
+	# Keep track of sample number (to ensure a certain no. of samples between each beat)
 	sample_num = 0
-	prev_sample_num = 0
+	prev_sample_num = sample_num
 
 	timestep = 30
 	window_size = 10 # prediction occurs every <window_size> samples
@@ -34,8 +36,9 @@ def read_serial(model, HAND):
 		"3-0","3-1","3-2","3-3","3-4",
 		"4-0","4-1","4-2","4-3","4-4"
 	]
-	BEAT = False
-	sound_activated = False
+	BEAT = False # 
+	BEAT_ACTIVATiED = False # 
+	BEAT_INTERVAL = 60 # number of samples between each beat (to prevent double-triggering)
 	LOW = 100
 	HIGH = 1000
 
@@ -45,42 +48,58 @@ def read_serial(model, HAND):
 			continue
 		try:
 			string = line.decode()	# convert the byte string to a unicode string
+			# print(string, end="")
 			if prev_gesture != gesture:
 				# print(gesture)
 				prev_gesture = gesture
-			# print(string, end="")
+
+			# Parse data into 5 values for flex sensors + accX
 			lst = [float(k) for k in string.split(',')[:-1]]
 			if len(lst) != 5:
 				continue
 			sample_num += 1
-
-			if HAND == "right":
-				data["index"].append(lst[0])
-				data["middle"].append(lst[1])
-				data["ring"].append(lst[2])
-				data["pinky"].append(lst[3])
-			else:
-				data["index"].append(lst[3])
-				data["middle"].append(lst[2])
-				data["ring"].append(lst[1])
-				data["pinky"].append(lst[0])
-
+			data["index"].append(lst[0])
+			data["middle"].append(lst[1])
+			data["ring"].append(lst[2])
+			data["pinky"].append(lst[3])
+		
 			accX = lst[4]
 
+			# If next beat is ready to be triggered
 			if not BEAT:
-				if accX > HIGH and not sound_activated:
-					sound_activated = True
-					prev_sample_num = sample_num
-				if sound_activated and accX < LOW:
-					ind = int(gesture[2])
-					BEAT = True
-					t = threading.Thread(target=play_sound, args=[ind])
-					t.start()
-					sound_activated = False
+				# Right hand (accX goes up then down)
+				if HAND == "right":
+					# Activate beat to be sounded
+					if accX > HIGH and not BEAT_ACTIVATED:
+						BEAT_ACTIVATED = True
+						prev_sample_num = sample_num
+
+					# Actually sound beat
+					if BEAT_ACTIVATED and accX < LOW:
+						BEAT = True
+						BEAT_ACTIVATED = False # reset to False to allow next activation of beat
+						ind = int(gesture[2]) # gesture index
+						t = threading.Thread(target=play_sound, args=[ind])
+						t.start()
+				# Left hand (accX goes down then up)
+				else:
+					if accX < -HIGH and not BEAT_ACTIVATED:
+						BEAT_ACTIVATED = True
+						prev_sample_num = sample_num
+					if BEAT_ACTIVATED and accX > -LOW:
+						BEAT = True
+						BEAT_ACTIVATED = False
+						ind = int(gesture[2])
+						t = threading.Thread(target=play_sound, args=[ind])
+						t.start()
+				
+			# Ensure that there're 60 samples between each beat
+			# Prevents double triggering
 			else:
-				if sample_num - prev_sample_num > 60:
+				if sample_num - prev_sample_num > BEAT_INTERVAL:
 					BEAT = False
 
+			# Parse data and pre-process for input to model
 			if len(data["index"]) == timestep:
 				x = np.c_[
 					np.array(data["index"]).reshape(-1,1),
@@ -90,13 +109,13 @@ def read_serial(model, HAND):
 					]
 				x = uniform_filter1d(x, axis=0, size=10)
 				for k in range(4):
-					# x[:,k] = x[:,k] - np.mean(x)
 					data_min, data_max = np.min(x), np.max(x)
 					x[:,k] = (x[:,k] - data_min) / (data_max - data_min)
 				x = np.ravel(x, order="F").reshape(1,-1)
 				for k,v in data.items():
 					data[k] = v[window_size:]	# prediction occurs every 10 samples
 
+				# Get model prediction
 				y_pred = model.predict(x, verbose=0)[0]
 				if max(y_pred) > 0.90:
 					transition = np.argmax(y_pred)
@@ -107,11 +126,12 @@ def read_serial(model, HAND):
 		except:
 			pass
 
+# Play the sound according to the gesture
 beat_count = 1
 def play_sound(ind):
 	global SOUNDS, beat_count
-	keyboard.press_and_release('a')
-	# SOUNDS[ind].play()
+	# keyboard.press_and_release('a')
+	SOUNDS[ind].play()
 	print("BEAT %d: %s     " % (beat_count, GESTURES[ind]), end="\r")
 	beat_count += 1
 	return
@@ -126,16 +146,15 @@ args = parser.parse_args()
 SERIAL_PATH = "/dev/ttyUSB" + str(args.dev)
 BAUD_RATE = 115200
 HAND = "left" if args.hand == "l" else "right"
+MODEL_PATH = "models/mlp_right" if HAND == "right" else "models/mlp_left"
 GESTURES = ["Kick","Hihat","Snare","Tom","Crash"]
 SOUNDS = [pyglet.resource.media("samples/" + file + ".wav", streaming=False)
 		for file in ["kick","hihat","snare","tom","crash"]
 			]
-# sound = pyglet.resource.media("trap_snare.wav", streaming=False)
 
 event = threading.Event()
-model = keras.models.load_model('mlp')
+model = keras.models.load_model(MODEL_PATH)
 serial = serial.Serial(SERIAL_PATH, BAUD_RATE, timeout=1)
-
 event.set()
 try:
 	print("Ready.")
